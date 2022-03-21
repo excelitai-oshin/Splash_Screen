cec88ecc3cfc94cffb87b174f7f7f01a7eabe204
Mac Mini <macmini@Macs-Mac-mini-7.local>
2022-03-21 14:32:21 +0600

    screen

---
+++ lib/content_model.dart
@@ -0,0 +1,35 @@
+class UnbordingContent{
+  String image;
+  String title;
+  String discription;
+
+  UnbordingContent({required this.image,required this.title,required this.discription});
+
+}
+List<UnbordingContent> contents=[
+  UnbordingContent(
+    title: "Quality Food",
+    image: "assets/vector1.png",
+    discription: "simple dummy text of the printing and type setting industry."
+    "Lorem Ipsum has been the industry's standard dummy text ever since"
+    "the 1500s, when an unknow printer took gallery of type and scrambled it",
+
+  ),
+  UnbordingContent(
+    title: "Quality Food",
+    image: "assets/vector2.png",
+    discription: "simple dummy text of the printing and type setting industry."
+        "Lorem Ipsum has been the industry's standard dummy text ever since"
+        "the 1500s, when an unknow printer took gallery of type and scrambled it",
+
+  ),
+  UnbordingContent(
+    title: "Quality Food",
+    image: "assets/vector3.png",
+    discription: "simple dummy text of the printing and type setting industry."
+        "Lorem Ipsum has been the industry's standard dummy text ever since"
+        "the 1500s, when an unknow printer took gallery of type and scrambled it",
+
+  ),
+
+];
